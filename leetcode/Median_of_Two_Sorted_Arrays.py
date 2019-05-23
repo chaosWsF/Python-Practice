c2031be12fd78288e24bd064d9498335a60af6bc
welkinXZ <25746440+chaosWsF@users.noreply.github.com>
2019-05-23 22:04:10 +0800

    add the simple structure of problem 4

---
+++ leetcode/Median_of_Two_Sorted_Arrays.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+
+        return True
+
+
+if __name__ == '__main__':
+    input1 = [1, 3]
+    input2 = [2]
+    sol = Solution()
+
+    print(sol.findMedianSortedArrays(input1, input2))
