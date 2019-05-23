5fb527259e915c729e4ef7c3714146dc07ec07a3
welkinXZ <25746440+chaosWsF@users.noreply.github.com>
2019-05-23 22:40:15 +0800

    add a sol of problem 4

--- leetcode/Median_of_Two_Sorted_Arrays.py
+++ leetcode/Median_of_Two_Sorted_Arrays.py
@@ -1,7 +1,24 @@
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
-
-        return True
+        if len(nums1) < len(nums2):
+            tmp = nums1
+            nums1 = nums2
+            nums2 = tmp
+        
+        start = (len(nums1) - 1) // 2
+        for bi in nums2:
+            while nums1[start] != bi:
+                if nums1[start] < bi:
+                    start = (start + len(nums1)) // 2
+                elif nums1[start] > bi:
+                    start = (start - 1) // 2
+        
+        if len(nums1) % 2 == 0:
+            result = (nums1[len(nums1) // 2 - 1] + nums1[len(nums1) // 2]) / 2
+        else:
+            result = nums1[len(nums1) // 2]
+        
+        return result
 
 
 if __name__ == '__main__':
