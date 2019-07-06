e6f0539e58b3a3a4306bb427b02fb730616d7634
welkinXZ <25746440+chaosWsF@users.noreply.github.com>
2019-07-06 13:28:38 +0800

    add more codes

--- leetcode/Median_of_Two_Sorted_Arrays.py
+++ leetcode/Median_of_Two_Sorted_Arrays.py
@@ -1,22 +1,34 @@
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
-        if len(nums1) < len(nums2):
+        n1 = len(nums1)
+        n2 = len(nums2)
+        if n1 < n2:
             tmp = nums1
             nums1 = nums2
             nums2 = tmp
         
-        start = (len(nums1) - 1) // 2
+        start = 0
+        end = n1 - 1
         for bi in nums2:
-            while nums1[start] != bi:
-                if nums1[start] < bi:
-                    start = (start + len(nums1)) // 2
-                else:
-                    start = (start - 1) // 2
+            # TODO add if statement to deal with bi overflow
+            if bi <= nums1[0]:
+                pass
+            elif bi >= nums1[-1]:
+                pass
+            else:
+                mid = (start + end) // 2
+                while (nums1[mid] != bi) and (end - start > 1):
+                    if nums1[mid] < bi:
+                        start = mid + 1
+                        end = end
+                        mid = (start + end) // 2
+                    else:
+                        start = (start - 1) // 2
         
-        if len(nums1) % 2 == 0:
-            result = (nums1[len(nums1) // 2 - 1] + nums1[len(nums1) // 2]) / 2
+        if (n1 + n2) % 2 == 0:
+            result = (nums1[(n1 + n2) // 2 - 1] + nums1[(n1 + n2) // 2]) / 2
         else:
-            result = nums1[len(nums1) // 2]
+            result = nums1[(n1 + n2) // 2]
         
         return result
 
