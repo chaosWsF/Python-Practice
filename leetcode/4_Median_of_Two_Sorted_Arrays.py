5f733aeb693aeee6eef853674d9e36d8403d94e6
Fang Wolf's <25746440+chaosWsF@users.noreply.github.com>
2019-12-23 21:32:14 +0800

    Solution of Problem 4

--- leetcode/4_Median_of_Two_Sorted_Arrays.py
+++ leetcode/4_Median_of_Two_Sorted_Arrays.py
@@ -1,41 +1,23 @@
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
-        n1 = len(nums1)
-        n2 = len(nums2)
-        if n1 < n2:
-            tmp = nums1
-            nums1 = nums2
-            nums2 = tmp
         
-        start = 0
-        end = n1 - 1
-        for bi in nums2:
-            # TODO add if statement to deal with bi overflow
-            if bi <= nums1[0]:
-                pass
-            elif bi >= nums1[-1]:
-                pass
-            else:
-                mid = (start + end) // 2
-                while (nums1[mid] != bi) and (end - start > 1):
-                    if nums1[mid] < bi:
-                        start = mid + 1
-                        end = end
-                        mid = (start + end) // 2
-                    else:
-                        start = (start - 1) // 2
-        
-        if (n1 + n2) % 2 == 0:
-            result = (nums1[(n1 + n2) // 2 - 1] + nums1[(n1 + n2) // 2]) / 2
-        else:
-            result = nums1[(n1 + n2) // 2]
-        
+        nums_all = nums1 + nums2
+
+        nums_all = sorted(nums_all)
+
+        mul = (len(nums_all) + 1) // 2
+        re = (len(nums_all) + 1) % 2
+        mid = [mul - 1, mul + re - 1]
+        result = (nums_all[mid[0]] + nums_all[mid[1]]) / 2
+
         return result
 
 
 if __name__ == '__main__':
-    input1 = [1, 3]
-    input2 = [2]
+    # input1 = [1, 3]
+    # input2 = [2]
+    input1 = [1, 2]
+    input2 = [3, 4]
     sol = Solution()
 
     print(sol.findMedianSortedArrays(input1, input2))
