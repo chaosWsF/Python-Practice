5019f831ff72963de8e155a3557a2afcbee03340
Fang Wolf's <25746440+chaosWsF@users.noreply.github.com>
2019-12-25 02:40:29 +0800

    upload problem 5

--- leetcode/longest_palindromic_substring.py
+++ leetcode/longest_palindromic_substring.py
@@ -1,10 +1,39 @@
 class Solution:
     def longestPalindrome(self, s):
         
+        # n = len(s)
+        # if s == s[::-1]:
+        #     result = s
+        # else:
+        #     result = s[0]
+        
+        # p = (n + 1) / 2
+        # i = int(p)
+        # while (1 < p < n) and (len(result) <= i):
+        #     j = 0
+        #     while j <= n - i:
+        #         s_ij = s[j:j+i]
+        #         if s_ij == s_ij[::-1]:
+        #             result = s_ij
+        #             p = (n + p) / 2
+        #             i = int(p)
+        #             break
+                
+        #         if j == n - i:
+        #             p = (p + 1) / 2
+        #             i = int(p)
+        #             break
+
+        #         j += 1
+        
+        # return result
+
         n = len(s)
-        # TODO: change to bisection
-        for i in range(n, 0, -1):
-            for j in range(0, n - i + 1):
-                s_ij = s[j:j+i]
-                if s_ij == s_ij[::-1]:
-                    return s_ij
+        if n > 0:
+            for i in range(n, 0, -1):
+                for j in range(0, n - i + 1):
+                    s_ij = s[j:j+i]
+                    if s_ij == s_ij[::-1]:
+                        return s_ij
+        else:
+            return s
--- leetcode/test_run.py
+++ leetcode/test_run.py
@@ -1,13 +1,15 @@
-from time import perf_counter
+import time
 from longest_palindromic_substring import Solution
 
 if __name__ == "__main__":
 
-    input = 'babad'
+    # input = 'babad'
     # input = 'cbbd'
-    # input = 'cbd'
+    input = 'abacab'
+    # input = 'abcda'
+    # input = 'adam'
 
     sol = Solution()
-    t0 = perf_counter()
+    t0 = time.perf_counter()
     print(sol.longestPalindrome(input))
-    print(perf_counter() - t0)
+    print(time.perf_counter() - t0)
