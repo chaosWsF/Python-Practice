16f0b67881867522b7404669e0ee751fee15fc04
Fang Wolf's <25746440+chaosWsF@users.noreply.github.com>
2019-12-26 02:17:47 +0800

    add a solution for palindromic

--- leetcode/longest_palindromic_substring.py
+++ leetcode/longest_palindromic_substring.py
@@ -1,32 +1,5 @@
 class Solution:
     def longestPalindrome(self, s):
-        
-        # n = len(s)
-        # if s == s[::-1]:
-        #     result = s
-        # else:
-        #     result = s[0]
-        
-        # p = (n + 1) / 2
-        # i = int(p)
-        # while (1 < p < n) and (len(result) <= i):
-        #     j = 0
-        #     while j <= n - i:
-        #         s_ij = s[j:j+i]
-        #         if s_ij == s_ij[::-1]:
-        #             result = s_ij
-        #             p = (n + p) / 2
-        #             i = int(p)
-        #             break
-                
-        #         if j == n - i:
-        #             p = (p + 1) / 2
-        #             i = int(p)
-        #             break
-
-        #         j += 1
-        
-        # return result
 
         n = len(s)
         if n > 0:
@@ -37,3 +10,5 @@ class Solution:
                         return s_ij
         else:
             return s
+
+# Manacher's algorithm can be O(n)
