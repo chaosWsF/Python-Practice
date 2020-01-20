76f633dbce61a25b537f3c52ad854d5d3b3ef942
Fang Wolf's <25746440+chaosWsF@users.noreply.github.com>
2020-01-20 18:51:45 +0800

    modify problem 1

--- leetcode/two_sum.py
+++ leetcode/two_sum.py
@@ -2,7 +2,7 @@ class Solution:
     def twoSum(self, nums, target):
         d = {}
         for i, num in enumerate(nums):
-            if num in d.keys():
+            if num in d:
                 return [d[num], i]
             
             d[target - num] = i
