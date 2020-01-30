5e12552c68d04643711f61bbf1d50cf8f9979e2e
Fang Wolf's <25746440+chaosWsF@users.noreply.github.com>
2020-01-30 20:26:27 +0800

    add leetcode problem 997 sol 1

---
+++ leetcode/find_the_town_judge.py
@@ -0,0 +1,72 @@
+"""
+In a town, there are N people labelled from 1 to N. There is a 
+rumor that one of these people is secretly the town judge.
+
+If the town judge exists, then:
+
+    The town judge trusts nobody
+    Everybody (except for the town judge) trusts the town judge
+    There is exactly one person that satisfies properties 1 and 2
+
+You are given trust, an array of pairs trust[i] = [a, b] 
+representing that the person labelled a trusts the person 
+labelled b.
+
+If the town judge exists and can be identified, return the label 
+of the town judge. Otherwise, return -1.
+
+Example 1:
+
+    Input: N = 2, trust = [[1,2]]
+    Output: 2
+
+Example 2:
+
+    Input: N = 3, trust = [[1,3],[2,3]]
+    Output: 3
+
+Example 3:
+
+    Input: N = 3, trust = [[1,3],[2,3],[3,1]]
+    Output: -1
+
+Example 4:
+
+    Input: N = 3, trust = [[1,2],[2,3]]
+    Output: -1
+
+Example 5:
+
+    Input: N = 4, trust = [[1,3],[1,4],[2,3],[2,4],[4,3]]
+    Output: 3
+
+Note:
+
+    1 <= N <= 1000
+    trust.length <= 10000
+    trust[i] are all different
+    trust[i][0] != trust[i][1]
+    1 <= trust[i][0], trust[i][1] <= N
+"""
+
+
+class Solution:
+    def findJudge(self, N, trust):
+        """direct modelling a graph"""
+        vertex = set(range(1, N+1))
+        edge_out = []
+        edge_in = []
+        for edge in trust:
+            edge_out.append(edge[0])
+            edge_in.append(edge[1])
+        edge_zero_outdegree = vertex.difference(edge_out)
+        
+        if len(edge_zero_outdegree) == 1:
+            town_judge = edge_zero_outdegree.pop()
+        else:
+            return -1
+        
+        if edge_in.count(town_judge) == N - 1:
+            return town_judge
+        else:
+            return -1
