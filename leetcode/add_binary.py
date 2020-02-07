95044f53392099d407afaaa60fd172c5afdcd61b
Fang Wolf's <25746440+chaosWsF@users.noreply.github.com>
2020-02-07 23:20:20 +0800

    add a sol of leet prob 67

--- leetcode/add_binary.py
+++ leetcode/add_binary.py
@@ -17,6 +17,36 @@ Example 2:
 
 
 class Solution:
-    def addBinary1(self, a: str, b: str) -> str:
-        """Convert to int (binary)"""
+    def addBinary1(self, a, b):
+        """Convert into binary integer (builtin)"""
         return bin(int(a, 2) + int(b, 2))[2:]
+
+    def addBinary2(self, a, b):
+        """Convert into digit adding"""
+        if len(a) < len(b):
+            tmp = a
+            a = b
+            b = tmp
+        
+        c = ''
+        i = 1
+        carrier = 0
+        while i <= len(a):
+            if i <= len(b):
+                d = int(a[-i]) + int(b[-i]) + carrier
+            else:
+                d = int(a[-i]) + carrier
+            
+            if d >= 2:
+                c += str(d - 2)
+                carrier = 1
+            else:
+                c += str(d)
+                carrier = 0
+
+            i += 1
+        
+        if carrier == 0:
+            return c[::-1]
+        else:
+            return '1' + c[::-1]
