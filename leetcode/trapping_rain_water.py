9542b7d9640cc517f6e2150e79d63c89bd9ed895
Fang Wolf's <25746440+chaosWsF@users.noreply.github.com>
2020-02-16 22:03:32 +0800

    add a sol of leet 42

--- leetcode/trapping_rain_water.py
+++ leetcode/trapping_rain_water.py
@@ -3,12 +3,6 @@ Given n non-negative integers representing an elevation map where
 the width of each bar is 1, compute how much water it is able to 
 trap after raining.
 
-
-The above elevation map is represented by array 
-[0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue
-section) are being trapped. Thanks Marcos for contributing this 
-image!
-
 Example:
 
     Input: [0,1,0,2,1,0,1,3,2,1,2,1]
@@ -18,24 +12,24 @@ Example:
 
 class Solution:
     def trap(self, height):
-        """Stack"""
+        """Two pointers, stack (44ms)"""
         if not height:
             return 0
         
         area = 0
-        stack = []
-        for h in height:
-            if not stack:
-                stack.append(h)
-            elif len(stack) == 1:
-                if stack[0] <= h:
-                    stack = [h]
-                else:
-                    stack.append(h)
-            elif h < stack[0]:
-                stack.append(h)
+        i = 0
+        j = 1
+        trapped = []
+        while j < len(height):
+            if height[i] <= height[j]:
+                area += len(trapped) * height[i] - sum(trapped)
+                i = j
+                trapped = []
             else:
-                area += len(stack) * stack[0] - sum(stack)
-                stack = [h]
+                trapped.append(height[j])
+            j += 1
+        else:
+            if trapped:
+                area += self.trap([max(trapped)] + trapped)
         
         return area
