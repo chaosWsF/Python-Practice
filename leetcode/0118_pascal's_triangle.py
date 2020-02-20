b0230be7031a22cac978456f8fe7071dfa7695f4
Fang Wolf's <25746440+chaosWsF@users.noreply.github.com>
2020-02-20 18:47:53 +0800

    add sols

--- leetcode/0118_pascal's_triangle.py
+++ leetcode/0118_pascal's_triangle.py
@@ -18,5 +18,23 @@ Example:
 
 
 class Solution:
-    def generate(self, numRows):
-        pass
+    def generate1(self, numRows):
+        """recurrence"""        
+        result = []
+        for n in range(1, numRows + 1):
+            cur = [1] + [result[-1][i - 1] + result[-1][i] for i in range(1, (n - 1) // 2 + 1)]
+            result.append(cur + cur[:(n // 2)][::-1])
+        return result
+    
+    def generate2(self, numRows):
+        """recurrence (faster)"""      
+        """recurrence"""
+        if numRows == 0:
+            return []
+        
+        result = [[1]]
+        for n in range(2, numRows + 1):
+            cur = [1] + [result[-1][i - 1] + result[-1][i] for i in range(1, (n - 1) // 2 + 1)]
+            result.append(cur + cur[:(n // 2)][::-1])
+        
+        return result
