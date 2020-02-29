7250525fea9cab04eb4febe41dea76a0242fefbf
Fang Wolf's <25746440+chaosWsF@users.noreply.github.com>
2020-02-29 16:05:00 +0800

    add new problem and solutiions

---
+++ leetcode/0167_two_sum_ii_input_array_is_sorted.py
@@ -0,0 +1,43 @@
+"""
+Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
+
+Note:
+
+    Your returned answers (both index1 and index2) are not zero-based.
+    You may assume that each input would have exactly one solution and you may not use the same element twice.
+
+Example:
+
+    Input: numbers = [2,7,11,15], target = 9
+    Output: [1,2]
+    Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+"""
+
+
+class Solution:
+    def twoSum1(self, numbers, target):
+        """Hashtable (60 ms)"""
+        d = {}
+        for i, n in enumerate(numbers):
+            if n in d:
+                return [d[n], i + 1]
+            else:
+                d[target - n] = i + 1
+
+    def twoSum2(self, numbers, target):
+        """Two Pointers (56 ms)"""
+        i = 0
+        j = len(numbers) - 1
+        while i < j:
+            if numbers[i] + numbers[j] > target:
+                j -= 1
+            elif numbers[i] + numbers[j] == target:
+                return [i + 1, j + 1]
+            else:
+                i += 1
+    
+    def twoSum3(self, numbers, target):
+        """Binary Search"""
+        
