629860e1e5bd9ef457c7e3e166d43994a6bc026d
Zhangqian Xie <25746440+chaosWsF@users.noreply.github.com>
2020-05-14 14:48:05 +0800

    add a new problem and solutions

---
+++ leetcode/0520_detect_capital.py
@@ -0,0 +1,38 @@
+"""
+Given a word, you need to judge whether the usage of capitals in it is right or not.
+
+We define the usage of capitals in a word to be right when one of the following cases holds:
+
+    All letters in this word are capitals, like "USA".
+    All letters in this word are not capitals, like "leetcode".
+    Only the first letter in this word is capital, like "Google".
+
+Otherwise, we define that this word doesn't use capitals in a right way. 
+
+Example 1:
+
+    Input: "USA"
+    Output: True
+
+Example 2:
+
+    Input: "FlaG"
+    Output: False
+
+Note: The input will be a non-empty word consisting of uppercase and lowercase latin letters.
+"""
+
+
+class Solution:
+    def detectCapitalUse(self, word):     # 32ms
+        return word.islower() or word.isupper() or word.istitle()
+
+    def detectCapitalUse2(self, word):    # 24ms
+        if word[0].islower():
+            return word.islower()
+        elif len(word) < 3:
+            return True
+        elif word[1].isupper():
+            return word[2:].isupper()
+        else:
+            return word[2:].islower()
