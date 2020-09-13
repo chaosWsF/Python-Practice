3db9e06cb967287315a91efd0537e844f0bacd64
Zhangqian Xie <25746440+chaosWsF@users.noreply.github.com>
2020-09-13 18:22:53 +0800

    update solution

--- leetcode/1005_maximize_sum_of_array_after_k_negations.py
+++ leetcode/1005_maximize_sum_of_array_after_k_negations.py
@@ -1,18 +1,43 @@
+"""
+Given an array A of integers, we must modify the array in the following way: we choose an i and replace 
+A[i] with -A[i], and we repeat this process K times in total. (We may choose the same index i multiple 
+times.) Return the largest possible sum of the array after modifying it in this way.
+
+Example 1:
+
+    Input: A = [4,2,3], K = 1
+    Output: 5
+    Explanation: Choose indices (1,) and A becomes [4,-2,3].
+
+Example 2:
+
+    Input: A = [3,-1,0,2], K = 3
+    Output: 6
+    Explanation: Choose indices (1, 2, 2) and A becomes [3,1,0,2].
+
+Example 3:
+
+    Input: A = [2,-3,-1,5,-4], K = 2
+    Output: 13
+    Explanation: Choose indices (1, 4) and A becomes [2,3,-1,5,4].
+
+Note:
+    1. 1 <= A.length <= 10000
+    2. 1 <= K <= 10000
+    3. -100 <= A[i] <= 100
+"""
+
+
 class Solution:
-    def largestSumAfterKNegations(self, A, K):
-
-        A = sorted(A)
+    def largestSumAfterKNegations1(self, A, K):    # 32ms
+        A.sort()
+        n = len(A)
+        if K <= n and A[K-1] < 0:
+            return sum(A) - 2 * sum(A[:K])
         
-        try:
-            if A[K-1] < 0:  # K <= num(A<0)
-                return sum(A) - 2 * sum(A[:K])
-        except IndexError:  # K > len(A)
-            pass
-
-        for i, a in enumerate(A):
-            if a < 0:
-                A[i] = -a
-            else:
-                break
-
-        return sum(A) - ((K-i) % 2) * 2 * min(A)
+        i = 0
+        while i < n and A[i] < 0:
+            A[i] = -A[i]
+            i += 1
+        
+        return sum(A) - (K - i) % 2 * min(A) * 2
