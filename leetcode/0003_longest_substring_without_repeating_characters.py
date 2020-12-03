8ad9584364ecaf8c252cef7c9d0b72104497819b
Zhangqian Xie <25746440+chaosWsF@users.noreply.github.com>
2020-12-03 21:31:19 +0800

    update solutions

--- leetcode/0003_longest_substring_without_repeating_characters.py
+++ leetcode/0003_longest_substring_without_repeating_characters.py
@@ -1,59 +1,60 @@
+"""
+Given a string s, find the length of the longest substring without repeating characters.
+
+Example 1:
+
+    Input: s = "abcabcbb"
+    Output: 3
+    Explanation: The answer is "abc", with the length of 3.
+
+Example 2:
+
+    Input: s = "bbbbb"
+    Output: 1
+    Explanation: The answer is "b", with the length of 1.
+
+Example 3:
+
+    Input: s = "pwwkew"
+    Output: 3
+    Explanation: The answer is "wke", with the length of 3.
+    Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+Example 4:
+
+    Input: s = ""
+    Output: 0
+
+Constraints:
+    1. 0 <= s.length <= 5 * 104
+    2. s consists of English letters, digits, symbols and spaces.
+"""
+
+
 class Solution:
-    def lengthOfLongestSubstring(self, s):
-        if uniqueOrder(s) in s:
-            return len(set(s))
-        else:
-            if len(set(s)) <= 2:
-                return len(set(s))
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        start = res = 0
+        char_loc = {}
+        for i in range(len(s)):
+            if char_loc.get(s[i], -1) >= start:
+                start = char_loc[s[i]] + 1
             else:
-                result = len(s)
-                s_list = [s]
-                for _ in range(len(s)):
-                    if not decideLoop(s_list, result):
-                        result -= 1
-                        s_list = [s[i:i+result] for i in range(len(s)-result+1)]
-                    else:
-                        result = decideLoop(s_list, result)
-                        break
-                
-                return result
+                res = max(res, i - start + 1)
 
+            char_loc[s[i]] = i
 
-def uniqueOrder(s):
-    seen = {}
-    ss_list = []
-    for ss in s:
-        if ss in seen:
-            continue
-        seen[ss] = 1
-        ss_list.append(ss)
+        return res
 
-    return ''.join(ss_list)
-
-
-def decideLoop(strList, result):
-    for s in strList:
-        if len(set(s)) != result:
-            continue
-        else:
-            return len(set(s))
-    
-    return False
-
-# =============================================================
-# class Solution:
-#     # @return an integer
-#     def lengthOfLongestSubstring(self, s):
-#         start = maxLength = 0
-#         usedChar = {}
+    def lengthOfLongestSubstring2(self, s: str) -> int:
+        if len(s) < 2:
+            return len(s)
         
-#         for i in range(len(s)):
-#             if s[i] in usedChar and start <= usedChar[s[i]]:
-#                 start = usedChar[s[i]] + 1
-#             else:
-#                 maxLength = max(maxLength, i - start + 1)
-
-#             usedChar[s[i]] = i
-
-#         return maxLength
-# =============================================================
+        ss, res = '', 0
+        for i in range(len(s)):
+            if s[i] not in ss:
+                ss += s[i]
+            else:
+                res = max(res, len(ss))
+                ss = ss[ss.find(s[i])+1:] + s[i]
+        
+        return max(res, len(ss))
