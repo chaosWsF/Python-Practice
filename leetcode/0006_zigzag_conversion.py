728be85cb781feb08fe30ebd2b0934262f2c7259
Zhangqian Xie <25746440+chaosWsF@users.noreply.github.com>
2020-12-07 19:18:15 +0800

    update description

--- leetcode/0006_zigzag_conversion.py
+++ leetcode/0006_zigzag_conversion.py
@@ -1,9 +1,50 @@
+"""
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: 
+(you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+    string convert(string s, int numRows);
+
+Example 1:
+
+    Input: s = "PAYPALISHIRING", numRows = 3
+    Output: "PAHNAPLSIIGYIR"
+
+Example 2:
+
+    Input: s = "PAYPALISHIRING", numRows = 4
+    Output: "PINALSIGYAHRPI"
+    Explanation:
+        P     I    N
+        A   L S  I G
+        Y A   H R
+        P     I
+
+Example 3:
+
+    Input: s = "A", numRows = 1
+    Output: "A"
+
+Constraints:
+    1. 1 <= s.length <= 1000
+    2. s consists of English letters (lower-case and upper-case), ',' and '.'.
+    3. 1 <= numRows <= 1000
+"""
+
+
 class Solution:
-    def convert(self, s, numRows):
-        if (numRows >= len(s)) or (numRows < 2):
+    def convert(self, s: str, numRows: int) -> str:    # 32ms, 100%
+        if (numRows >= len(s)) or (numRows == 1):
             return s
         
-        m = 2 * numRows - 2
+        m = 2 * (numRows - 1)
         k = len(s) // m
         if k > 0:
             q = len(s) % m
@@ -19,17 +60,14 @@ class Solution:
                 elif (q > numRows) and (m - q + 1 <= i <= q - 1):
                     row += s[i+k*m] + s[m-i+k*m]
 
-            row += s[numRows-1::m]
-
+            row += s[(numRows-1)::m]
             return row
-        
         else:
             row = s[0]
             for i in range(1, numRows - 1):
                 row += s[i]
-                if m-i < len(s):
+                if m - i < len(s):
                     row += s[m-i]
 
             row += s[numRows-1]
-            
             return row
