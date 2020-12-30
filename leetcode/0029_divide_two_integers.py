ffe0c50b447521c9e92e731175f19411b39e17aa
zero <25746440+chaosWsF@users.noreply.github.com>
2020-12-30 12:53:40 +0800

    update solutions

--- leetcode/0029_divide_two_integers.py
+++ leetcode/0029_divide_two_integers.py
@@ -1,6 +1,6 @@
 """
-Given two integers dividend and divisor, divide two integers 
-without using multiplication, division and mod operator.
+Given two integers dividend and divisor, divide two integers without using 
+multiplication, division and mod operator.
 
 Return the q after dividing dividend by divisor.
 
@@ -17,23 +17,23 @@ Example 2:
     Output: -2
 
 Note:
-
-    Both dividend and divisor will be 32-bit signed integers.
-    The divisor will never be 0.
-    Assume we are dealing with an environment which could only 
-    store integers within the 32-bit signed integer range: 
-    [−2^31, 2^31 − 1]. For the purpose of this problem, assume 
-    that your function returns 2^31 − 1 when the division result 
-    overflows.
+    1. Both dividend and divisor will be 32-bit signed integers.
+    2. The divisor will never be 0.
+    3. Assume we are dealing with an environment which could only store integers 
+       within the 32-bit signed integer range: [−2^31, 2^31 − 1]. For the purpose 
+       of this problem, assume that your function returns 2^31 − 1 when the division 
+       result overflows.
 """
 
 
 class Solution:
-    def divide1(self, dividend, divisor):
-        """Use Range Stepwise"""
+    def divide1(self, dividend: int, divisor: int) -> int:
+        """
+        Using stepwise range()
+        """
         sig = (dividend > 0) ^ (divisor > 0)
-        dividend = abs(dividend)
-        divisor = abs(divisor)
+        dividend, divisor = abs(dividend), abs(divisor)
+
         if divisor == 1:
             result = dividend
         else:
@@ -44,11 +44,12 @@ class Solution:
         else:
             return min(result, 2**31 - 1)
 
-    def divide2(self, dividend, divisor):
-        """While Loop (Bitwise Operation)"""
+    def divide2(self, dividend: int, divisor: int) -> int:
+        """
+        Bitwise Operation
+        """
         sig = (dividend > 0) ^ (divisor > 0)
-        dividend = abs(dividend)
-        divisor = abs(divisor)
+        dividend, divisor = abs(dividend), abs(divisor)
 
         q = 0
         while dividend >= divisor:
@@ -61,8 +62,10 @@ class Solution:
         else:
             return min(q, 2**31 - 1)
 
-    def _bit_size(self, dividend, divisor):
-        """find max bit size"""
+    def _bit_size(self, dividend: int, divisor: int):
+        """
+        To find max bit size
+        """
         q = 1
         while divisor < dividend:
             divisor <<= 1
