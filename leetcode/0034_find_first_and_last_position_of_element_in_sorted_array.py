f77710219f897b8d955ab3b46d71bfa904fc6848
zero <25746440+chaosWsF@users.noreply.github.com>
2021-01-21 03:29:16 +0800

    add two solutions, not fastest

--- leetcode/0034_find_first_and_last_position_of_element_in_sorted_array.py
+++ leetcode/0034_find_first_and_last_position_of_element_in_sorted_array.py
@@ -30,5 +30,54 @@ Constraints:
 
 
 class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        pass
+    def searchRange1(self, nums: List[int], target: int) -> List[int]:
+        """
+        Binary search: O(log n), the worst case n + log n
+        Runtime: 84ms
+        """
+        if not nums:
+            return [-1, -1]
+        
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] == target:
+                break
+            else:
+                l = m + 1
+        
+        if nums[m] != target:
+            return [-1, -1]
+        
+        i = j = m
+        while i >= 0 and nums[i] == target:
+            i -= 1
+        
+        while j < len(nums) and nums[j] == target:
+            j += 1
+
+        return [i + 1, j - 1]
+
+    def searchRange2(self, nums: List[int], target: int) -> List[int]:
+        """
+        Linear search: O(n), the worst case 2*n
+        Runtime: 80ms
+        """        
+        l = -1
+        for i in range(0, len(nums)):
+            if nums[i] == target:
+                l = i
+                break
+        
+        if l == -1:
+            return [-1, -1]
+        
+        r = -1
+        for j in range(len(nums)-1, -1, -1):
+            if nums[j] == target:
+                r = j
+                break
+        
+        return [l, r]
