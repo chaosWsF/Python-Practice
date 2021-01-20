a80763b0a8f632e156045f547c1b1de3a24a11cd
zero <25746440+chaosWsF@users.noreply.github.com>
2021-01-20 15:45:17 +0800

    add a new problem

---
+++ leetcode/0034_find_first_and_last_position_of_element_in_sorted_array.py
@@ -0,0 +1,34 @@
+"""
+Given an array of integers nums sorted in ascending order, find the starting 
+and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+Follow up: Could you write an algorithm with O(log n) runtime complexity?
+
+Example 1:
+
+    Input: nums = [5,7,7,8,8,10], target = 8
+    Output: [3,4]
+
+Example 2:
+
+    Input: nums = [5,7,7,8,8,10], target = 6
+    Output: [-1,-1]
+
+Example 3:
+
+    Input: nums = [], target = 0
+    Output: [-1,-1]
+
+Constraints:
+    1. 0 <= nums.length <= 105
+    2. -109 <= nums[i] <= 109
+    3. nums is a non-decreasing array.
+    4. -109 <= target <= 109
+"""
+
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        pass
