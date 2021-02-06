733f1a068590ad87f2f36b4b0578f48f3340cf12
zero <25746440+chaosWsF@users.noreply.github.com>
2021-02-06 14:20:51 +0800

    update testing module

--- Interview/test_with_examples.py
+++ Interview/test_with_examples.py
@@ -5,25 +5,41 @@ from main import Solution
 
 class TestExamples(unittest.TestCase):
     def test_eg1(self):
+        ex_dir = './Example1/{}'
+
         ex_files = [
             'courses.csv', 'students.csv', 'tests.csv', 'marks.csv', 
             'output_1.json'
         ]
-        ex_dir = './Example1/{}'
         sol = Solution([ex_dir.format(s) for s in ex_files])
-        sol.writing()
+        d = sol.combining()
+        res = json.dumps({'students': d}, indent=2)
+
+        with open(ex_dir.format('output.json')) as f:
+            expected_res = f.read()
+        
+        self.assertEqual(res, expected_res)
     
     def test_eg2(self):
+        ex_dir = './Example2/{}'
+
         ex_files = [
             'courses.csv', 'students.csv', 'tests.csv', 'marks.csv', 
             'output_1.json'
         ]
-        ex_dir = './Example2/{}'
         sol = Solution([ex_dir.format(s) for s in ex_files])
-        sol.writing()
+        d = sol.combining()
+        res = json.dumps({'students': d}, indent=2)
+
+        with open(ex_dir.format('output.json')) as f:
+            expected_res = f.read()
+        
+        self.assertEqual(res, expected_res)
 
 
 if __name__ == '__main__':
-    # unittest.main()
-    TestExamples().test_eg1()
-    TestExamples().test_eg2()
+
+    # TestExamples().test_eg1()
+    # TestExamples().test_eg2()
+
+    unittest.main()
