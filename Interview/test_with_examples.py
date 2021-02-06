e56e83d83fe7d6032884c261f4a7dc576cfbb98b
zero <25746440+chaosWsF@users.noreply.github.com>
2021-02-06 18:20:50 +0800

    add an example and testing results

--- Interview/test_with_examples.py
+++ Interview/test_with_examples.py
@@ -1,45 +1,71 @@
 import unittest
 import json
+
 from main import Solution
 
 
 class TestExamples(unittest.TestCase):
     def test_eg1(self):
+        """
+        Test specific fields
+        """
         ex_dir = './Example1/{}'
-
         ex_files = [
             'courses.csv', 'students.csv', 'tests.csv', 'marks.csv', 
-            'output_1.json'
+            'output_test.json'
         ]
-        sol = Solution([ex_dir.format(s) for s in ex_files])
-        d = sol.combining()
-        res = json.dumps({'students': d}, indent=2)
 
-        with open(ex_dir.format('output.json')) as f:
-            expected_res = f.read()
+        sol = Solution([ex_dir.format(s) for s in ex_files])
+        sol.writing()
+
+        with open(ex_dir.format(ex_files[-1])) as f:
+            res = json.load(f)
         
-        self.assertEqual(res, expected_res)
+        with open(ex_dir.format('output.json')) as f:
+            expected_res = json.load(f)
+            
+        self.assertEqual(res['students'][1]['totalAverage'], expected_res['students'][1]['totalAverage'])
+        self.assertEqual(res['students'][1]['courses'], expected_res['students'][1]['courses'])
     
     def test_eg2(self):
+        """
+        Test entire example 2
+        """
         ex_dir = './Example2/{}'
-
         ex_files = [
             'courses.csv', 'students.csv', 'tests.csv', 'marks.csv', 
-            'output_1.json'
+            'output_test.json'
         ]
-        sol = Solution([ex_dir.format(s) for s in ex_files])
-        d = sol.combining()
-        res = json.dumps({'students': d}, indent=2)
 
+        sol = Solution([ex_dir.format(s) for s in ex_files])
+        sol.writing()
+
+        with open(ex_dir.format(ex_files[-1])) as f:
+            res = json.load(f)
+        
         with open(ex_dir.format('output.json')) as f:
-            expected_res = f.read()
+            expected_res = json.load(f)
         
         self.assertEqual(res, expected_res)
+
+    def test_eg3(self):
+        """
+        Test outputing error when weights are incorrect
+        """
+        ex_dir = './Example3/{}'
+        ex_files = [
+            'courses.csv', 'students.csv', 'tests.csv', 'marks.csv', 
+            'output_test.json'
+        ]
+
+        sol = Solution([ex_dir.format(s) for s in ex_files])
+        sol.writing()
+
+        with open(ex_dir.format(ex_files[-1])) as f:
+            res = json.load(f)
+        
+        self.assertEqual(res, {"error": "Invalid course weights"})
 
 
 if __name__ == '__main__':
-
-    # TestExamples().test_eg1()
-    # TestExamples().test_eg2()
-
     unittest.main()
