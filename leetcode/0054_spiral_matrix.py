fa5b05aea7a1f5bcbb40c62ab8fe31fe88037b0f
zero <25746440+chaosWsF@users.noreply.github.com>
2021-02-12 14:14:21 +0800

    add a new medium problem

---
+++ leetcode/0054_spiral_matrix.py
@@ -0,0 +1,28 @@
+"""
+Given an m x n matrix, return all elements of the matrix in spiral order.
+
+Example 1:
+    
+    https://assets.leetcode.com/uploads/2020/11/13/spiral1.jpg
+
+    Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    Output: [1,2,3,6,9,8,7,4,5]
+
+Example 2:
+
+    https://assets.leetcode.com/uploads/2020/11/13/spiral.jpg
+
+    Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+    Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+
+Constraints:
+    1. m == matrix.length
+    2. n == matrix[i].length
+    3. 1 <= m, n <= 10
+    4. -100 <= matrix[i][j] <= 100
+"""
+
+
+class Solution:
+    def spiralOrder(self, matrix):
+        pass
