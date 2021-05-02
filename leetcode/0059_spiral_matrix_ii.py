bc24020887c9922ea0a0212d6f1dbcf5fae69641
zero <25746440+chaosWsF@users.noreply.github.com>
2021-05-02 16:44:13 -0600

    add a solution

--- leetcode/0059_spiral_matrix_ii.py
+++ leetcode/0059_spiral_matrix_ii.py
@@ -19,5 +19,37 @@ Constraints:
 
 
 class Solution:
-    def generateMatrix(self, n: int):
-        pass
+    def generateMatrix(self, n: int):    # 28ms
+        matrix = [[0 for _ in range(n)] for _ in range(n)]
+        x = 1
+
+        def helper(r0, c0, r1, c1, x0):
+            for c in range(c0, c1 + 1):
+                matrix[r0][c] = x0
+                x0 += 1
+            
+            for r in range(r0 + 1, r1 + 1):
+                matrix[r][c1] = x0
+                x0 += 1
+            
+            if r0 < r1 and c0 < c1:    # multi-row and multi-col
+                for c in range(c1 - 1, c0, -1):
+                    matrix[r1][c] = x0
+                    x0 += 1
+                
+                for r in range(r1, r0, -1):
+                    matrix[r][c0] = x0
+                    x0 += 1
+
+            return x0
+
+
+        r0, c0, r1, c1 = 0, 0, len(matrix)-1, len(matrix[0])-1
+        while r0 <= r1 and c0 <= c1:
+            x = helper(r0, c0, r1, c1, x)
+            r0 += 1
+            c0 += 1
+            r1 -= 1
+            c1 -= 1
+        
+        return matrix
