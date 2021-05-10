d75dbfd3cb6919e12c47dc99bebcf2aac0bcada1
wswordform <25746440+chaosWsF@users.noreply.github.com>
2021-05-10 00:43:12 -0600

    add solution

--- leetcode/0064_minimum_path_sum.py
+++ leetcode/0064_minimum_path_sum.py
@@ -26,4 +26,16 @@ Constraints:
 
 class Solution:
     def minPathSum(self, grid) -> int:
-        pass
+        m, n = len(grid), len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    pass
+                elif i == 0:
+                    grid[i][j] += grid[0][j-1]
+                elif j == 0:
+                    grid[i][j] += grid[i-1][0]
+                else:
+                    grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+
+        return grid[-1][-1]
